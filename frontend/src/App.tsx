3b9957d56feae689b953a6db56288cfefe50f2f1
GALBICCIM <godvictory0909@gmail.com>
2024-12-01 14:24:36 +0900

    INIT: Initialization

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import ImageUpload from "./components/ImageUpload";
+
+const App = () => {
+   return (
+      <div className="App">
+         <ImageUpload />
+      </div>
+   );
+};
+
+export default App;
